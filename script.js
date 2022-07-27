51102c0fe47204d1c4ca8209db8679e03eb64794
Carlos Garro <carlosgaro11@gmail.com>
2022-07-27 15:12:30 -0400

    Added the home page boxes for News, Social, Goals, and Feeling Lucky

---
+++ script.js
@@ -0,0 +1,10 @@
+// fetch('https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&apiKey=a09675119b4846298ec84a1fedd5f4a6')
+// .then((response) => response.json())
+// .then((data) => {
+//     let recipeArea = document.querySelector(".recipeTitle");
+//     let titleElement= document.createElement('p');
+//     let titleText = document.createTextNode(`${data.title}`);
+//     titleElement.appendChild(titleText);
+//     recipeArea.appendChild(titleElement);
+// })
+// ABOVE IS A EXAMPLE OF HOW I GOT THE DATA FROM THE API
